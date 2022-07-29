84f881cdcdf73cc729df446e9f39749debe5be6f
inosato <si17_21@yahoo.co.jp>
2022-07-29 23:41:19 +0900

    Remove ioutil
    Signed-off-by: inosato <si17_21@yahoo.co.jp>

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net"
 	"net/http"
 	"os"
@@ -150,7 +150,7 @@ func newCollectdCollector(logger log.Logger) *collectdCollector {
 }
 
 func (c *collectdCollector) collectdPost(w http.ResponseWriter, r *http.Request) {
-	data, err := ioutil.ReadAll(r.Body)
+	data, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
