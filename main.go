d32af3c6df046362a870187358e4704584f9ea33
Ben Kochie <superq@gmail.com>
2024-08-08 19:17:09 +0200

    Update Go modules (#161)
    Update prometheus/common and update version collector use.
    
    Signed-off-by: SuperQ <superq@gmail.com>

--- main.go
+++ main.go
@@ -32,6 +32,7 @@ import (
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
+	versioncollector "github.com/prometheus/client_golang/prometheus/collectors/version"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/promlog"
 	"github.com/prometheus/common/promlog/flag"
@@ -307,7 +308,7 @@ func startCollectdServer(ctx context.Context, w api.Writer, logger log.Logger) {
 }
 
 func init() {
-	prometheus.MustRegister(version.NewCollector("collectd_exporter"))
+	prometheus.MustRegister(versioncollector.NewCollector("collectd_exporter"))
 }
 
 func main() {
